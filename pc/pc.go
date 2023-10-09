b74ab8bd98bc7faf5782a1da97b73097eab03099
Harry Greentree <harrygreentree@icloud.com>
2023-10-09 15:52:31 +0100

    Question 2 - Producer-consumer problem

--- pc/pc.go
+++ pc/pc.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/ChrisGora/semaphore"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -33,27 +36,44 @@ func (buffer *buffer) put(x int) {
 	buffer.write = (buffer.write + 1) % len(buffer.b)
 }
 
-func producer(buffer *buffer, start, delta int) {
+func producer(buffer *buffer, spaceAvailable, workAvailable semaphore.Semaphore, mutex *sync.Mutex, start, delta int) {
 	x := start
 	for {
+		spaceAvailable.Wait()
+		mutex.Lock()
+
 		buffer.put(x)
 		x = x + delta
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+
+		mutex.Unlock()
+		workAvailable.Post()
 	}
 }
 
-func consumer(buffer *buffer) {
+func consumer(buffer *buffer, spaceAvailable, workAvailable semaphore.Semaphore, mutex *sync.Mutex) {
 	for {
+		workAvailable.Wait()
+		mutex.Lock()
+
 		_ = buffer.get()
 		time.Sleep(time.Duration(rand.Intn(5000)) * time.Millisecond)
+
+		mutex.Unlock()
+		spaceAvailable.Post()
 	}
 }
 
 func main() {
 	buffer := newBuffer(5)
 
-	go producer(&buffer, 1, 1)
-	go producer(&buffer, 1000, -1)
+	var mutex sync.Mutex
 
-	consumer(&buffer)
+	spaceAvailable := semaphore.Init(5, 5)
+	workAvailable := semaphore.Init(5, 0)
+
+	go producer(&buffer, spaceAvailable, workAvailable, &mutex, 1, 1)
+	go producer(&buffer, spaceAvailable, workAvailable, &mutex, 1000, -1)
+
+	consumer(&buffer, spaceAvailable, workAvailable, &mutex)
 }
